51d12d8de7cbe30cc3d303160879a27434eef859
Unknown <alexis_gavriel@hotmail.com>
2019-03-16 21:52:00 -0600

    Headlines ejercicio 3
    Se agregan autores
    Descripción del código
    Comentarios

--- tarea1/ejercicio3.m
+++ tarea1/ejercicio3.m
@@ -1,15 +1,41 @@
 1;
+%{
+Integrantes
+=========================
+Gladys Arias 
+Alexis Gavriel (Autor del código del ejercicio 3)
+Cristian Rivera 
+Andres Vargas
+Fecha: Marzo 15, 2019.
 
+Descripción del algoritmo
+=========================
+algoritmo: La funcion grafica las señales x1, x2, x3, x4 en una figura:
+  x1 = cos(pi*n/2);
+  x2 = sin(pi*n/8);
+  x3 = cos(pi*n/4 + pi/3);
+  x4 = cos(2*n/pi);
+
+además grafica las funciones y1, y2, y3 en otra figura:
+  y1 = x1 + x2;
+  y2 = x1 + x2 - x3;
+  y3 = x1 + x2 - x3 - 2*x4;
+
+  ** Todas las gráficas se hacen con 50 muestras (n = 50)
+%}
 #######################
 #
 
 function ejercicio3()
   n = 0:50;
+
+  % Crea cada funcion x1, x2, x3, x4
   x1 = cos(pi*n/2);
   x2 = sin(pi*n/8);
   x3 = cos(pi*n/4 + pi/3);
   x4 = cos(2*n/pi);
   
+  % Genera una figura en donde se muestran las 4 fuciones x1, x2, x3, x4
   figure(1);
   subplot(2,2,1);
     stem(n,x1);
@@ -25,6 +51,7 @@ function ejercicio3()
     title (sprintf ("x4"));
     
   %{
+    Análisis de las funciones x1, x2, x3, x4
     x1: se puede apreciar que los puntos maximos se dan en 4 y 8, 
       por lo que se determina que el periodo T = 4
     x2: se puede apreciar que los puntos minimos ocurren en 12 y 28
@@ -35,6 +62,8 @@ function ejercicio3()
     Estos puntos maximos no coinciden el uno con los siguientes.
   %}
   
+
+  % Se crea otra figura en donde se mostrarán las figuras y1, y2, y3
   figure(2);
   
   y1 = x1+x2;
@@ -52,6 +81,7 @@ function ejercicio3()
     title (sprintf ("y3"));
 
   %{
+    Análisis de las funciones y1, y2, y3
     y1: la funcion aparenta tener un periodo T = 16, ya que sus puntos
       maximos coinciden en 20 y 36
     y2: la funcion aparenta tener un periodo T = 16, ya que sus puntos
