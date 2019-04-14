204deb3de812a962bdb56734313e1bbe09844613
Alexis <alexis_gavriel@hotmail.com>
2019-04-14 17:24:43 -0600

    Enunciado + update problema3
    falta el punto C en cada grafica

--- tarea2/problema3.m
+++ tarea2/problema3.m
@@ -1,30 +1,60 @@
-1;
 
-function ejercicio3()
+
+
+%{
+
+a
+En general las señales x(n) y y(n) son parecidas. 
+Entre sus diferencias están:
+y(n) contiene ruido 
+Está retrasada en el tiempo por un factor D. 
+
+Al realizar la autocorrelación con un factor de K = 0 se obtiene el valor máximo de la autocorrelación.
+Debido a ello, se puede inducir que si se realiza la correlación entre x(n) y y(n) en un factor K tal que D = -K se obtiene:
+y(t) = ax(t -td + tk) + v(t)
+y(t) = ax(t) + v(t)
+
+
+%}
+
+
+function problema3()
 
 	n = 0:199;
 
+	%% se crea el vector original x(n) como señal del emisor
+
 	x_original = [1,1,1,1,1,-1,-1,1,1,-1,1,-1,1];
 
+	%% se agrega padding a la señal para que tenga un tamaño de 200
 	x = [x_original,zeros(1,187)];
+
+	%% se crea un vector de ruido de tamaño 200 con valor medio igual a 0 y varianza de 0.01
 	std = 0.1;
 	% mean = 0;
 	% v = std.*randn(1,200) + mean;
 
 	v = std.*randn(1,200);
 
+	%% se coloca un supuesto retraso de 20
 	D = zeros(1,20);
 
 	x_padded = [D,x_original,zeros(1,167)];
+
+	%% se genera la señal del receptor, igual a la señal emitida retrasada por 20 y con ruido
 	y = 0.9.*x_padded + v;
+
+	%----  b
+
 	figure(1);
 	subplot(2,1,1);	  
-	  stem(x_graph);
+	  stem(x);
 	  title (sprintf ("x(n)"));
 	subplot(2,1,2);
 	  stem(y);
-	  title (sprintf ("y(n)"));
+	  title (sprintf ("y(n), variance = 0.01"));
 
+	%---- c
 
 %{
 	PUNTO C CORRELACIÓN
@@ -40,16 +70,20 @@ function ejercicio3()
 
 
 % ---------------------------------------------------
+%---- d
 	% sigma^2 = 0.1   sigma^2 = 1
 
 	std_2 = sqrt(0.1);
 	std_3 = 1;
 	
+	%% se crea un vector de ruido de tamaño 200 con valor medio igual a 0 y varianza de 1 y 0.1
+
 	v_2 = std_2.*randn(1,200);
 	v_3 = std_3.*randn(1,200);
 
-	y_2 = 0.9.*x_padded + v_2;
-	y_3 = 0.9.*x_padded + v_3;
+	%% se genera la señal del receptor, igual a la señal emitida retrasada por 20 y con ruido
+	y_2 = 0.9.* x_padded + v_2;
+	y_3 = 0.9.* x_padded + v_3;
 
 	figure(2);
 	subplot(2,1,1);	 
@@ -64,19 +98,20 @@ function ejercicio3()
 
 
 
-% ---------------------------------------------------
-
+%---------------------------------------------------
+%---- e
 	x2_original = [-1,-1,-1,+1,+1,+1,+1,-1,+1,-1,+1,+1,-1,-1,+1];
 
 	x2 = [x2_original,zeros(1,185)];
-	x2_padded = [D,x2,zeros(1,165)];
+	x2_padded = [D,x2_original,zeros(1,165)];
 
-	y_x2 = 0.9.*x2_padded + v;
+	%% se genera la señal del receptor, igual a la señal emitida retrasada por 20 y con ruido
+	y_x2 = (0.9.*x2_padded )+ v;
 
-	figure(3);
+	figure(3);	
 	subplot(2,1,1);	 
 	  stem(x2);
-	  title (sprintf ("x2(n)"));
+	  title (sprintf ("x2(n), 4 register adder"));
 	subplot(2,1,2);
 	  stem(y_x2);
 	  title (sprintf ("y2(n)"));
@@ -85,6 +120,38 @@ function ejercicio3()
 
 % PUNTO C, variables: x2, y_x2  
 % si puede utilizar x_original es más eficiente
+
+%---------------------------------------------------
+%---- f
+	register7 = zeros(1,7);
+	register7(1) = 1;
+
+
+	x3 = zeros(1,127);
+
+	first = 0;
+	last = 0;
+
+	for i = 1:127
+		first = register7(1);
+		register7 = shift(register7,1);
+		last = register7(1);
+		x3(i) = last;
+		register7(1) = xor(first, last);
+	endfor
+
+	x3_padded = [x3,zeros(1,73)];
+	y_x3 = (0.9.*[D,x3,zeros(1,53)] )+ v;
+
+	figure(3);	
+	subplot(2,1,1);	 
+	  stem(x3_padded);
+	  title (sprintf ("x3(n), 7 register adder"));
+	subplot(2,1,2);
+	  stem(y_x3);
+	  title (sprintf ("y3(n)"));
+
+
 
 
 
