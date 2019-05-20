c08c9c14c4630edd310f99d3468b87d77db04faa
avargas10 <afelipe.vargas.r@gmail.com>
2019-05-19 23:43:03 -0600

    tarea3

---
+++ tarea3/problema1/ejercicio1.m
@@ -0,0 +1,10 @@
+figure (1);
+%muestra el primer diagrama de bloques para el ejericio 1
+freqz([1/2,1/2]);
+figure (2);
+%muestra el segundo diagrama de bloques para el ejericio 1
+freqz([1/2,-1/2]);
+figure (3);
+%muestra el tercer diagrama de bloques para el ejericio 1
+freqz([1/8,3/8,3/8,1/8]);
+
